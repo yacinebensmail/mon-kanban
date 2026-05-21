747334d2dc06dafcd26a73219597cda1b904ec65
Yacinebensmail <yacine.bensmail@etu.univ-st-etienne.fr>
2026-05-21 10:37:34 +0200

    Initialisation du projet Kanban

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
+      <h1>Mon premier déploiement</h1>
+      <p>Projet réalisé par <strong>Yacine Bensmaïl</strong></p>
+      <p>Future application gestion de tâches Kanban</p>
+    </div>
+  );
+}
+export default App;
