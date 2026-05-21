fee1c169ce385c4248302d588ec55e68397ce393
Yacinebensmail <yacine.bensmail@etu.univ-st-etienne.fr>
2026-05-21 10:41:27 +0200

    Test du déploiement automatique

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
-      <h1>Mon premier déploiement</h1>
+      <h1>Déploiement automatique opérationnel</h1>
       <p>Projet réalisé par <strong>Yacine Bensmaïl</strong></p>
       <p>Future application gestion de tâches Kanban</p>
     </div>
